4b7e2a59c4aed71cfe2918e819664b59e8f6f20b
Adedeji Onigbinde <adedejionigbinde@gmail.com>
2024-04-29 19:53:53 -0400

    Fixed Tabs On Transfer Page

--- BankFrontEnd/src/app/components/client-module/transfer/transfer.component.ts
+++ BankFrontEnd/src/app/components/client-module/transfer/transfer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TabItem, Tabs } from 'flowbite';
 
 @Component({
   selector: 'app-transfer',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransferComponent implements OnInit {
   transferTransactions: any[] = [];
+  tabsElement!: HTMLElement | null;
+  tabElements: TabItem[] = [];
+  tabs!: Tabs;
   constructor() { }
 
   ngOnInit(): void {
+    this.initializeTabs();
+  }
+
+  options = {
+    defaultTabId: 'domestic',
+    activeClasses:
+    'text-primaryGreen border-primaryGreen font-bold',
+  };
+
+  initializeTabs(): void {
+    this.tabsElement = document.getElementById('default-tab');
+    this.tabElements = [
+      {
+        id: 'domestic',
+        triggerEl: document.querySelector('#domesticTransfer-tab') as HTMLElement,
+        targetEl: document.querySelector('#domesticTransfer') as HTMLElement,
+      },
+      {
+        id: 'international',
+        triggerEl: document.querySelector('#internationalTransfer-tab') as HTMLElement,
+        targetEl: document.querySelector('#internationalTransfer') as HTMLElement,
+      },
+      {
+        id: 'interbank',
+        triggerEl: document.querySelector('#interbankTransfer-tab') as HTMLElement,
+        targetEl: document.querySelector('#interbankTransfer') as HTMLElement,
+      },
+    ];
+    this.tabs = new Tabs(this.tabsElement, this.tabElements, this.options);
   }
 
 }
